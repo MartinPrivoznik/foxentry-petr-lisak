6b65c2876492a96a3755bcd8bbe6eb842897fa75
Martin Přívozník <marprivoznik@gmail.com>
2024-09-12 18:04:28 +0200

    feat: prepared open api specs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './documentation/swagger_def.json';
 
 dotenv.config();
 
@@ -7,5 +9,7 @@ const app: Express = express();
 
 // Use JSON parser for all incoming requests
 app.use(express.json());
+
+app.use('/api-reference', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export default app;
