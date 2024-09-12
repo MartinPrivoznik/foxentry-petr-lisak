8740cdbba80928853d153ddaf72a784a5788e14d
Martin Přívozník <marprivoznik@gmail.com>
2024-09-12 11:08:18 +0200

    feat: linter setup

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  prettierConfig,
+  {
+    ignores: ['node_modules', 'dist', 'jest.config.js'],
+  }
+);
